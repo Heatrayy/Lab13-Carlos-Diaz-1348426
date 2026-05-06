12ef2136d60c5d42f6422d64371c832b13b29211
Heatrayy <115377416+Heatrayy@users.noreply.github.com>
2026-05-05 22:49:36 -0600

    Update Lab13- Ejercicio 1.cs

--- Lab13- Ejercicio 1.cs
+++ Lab13- Ejercicio 1.cs
@@ -1,5 +1,7 @@
 using System;
 
+using System;
+
 class Persona
 {
     public string nombre;
@@ -9,11 +11,29 @@ class Persona
 
     public void MostrarInformacion()
     {
-        Console.WriteLine("EJERCICIO 1 - PERSONA");
+        Console.WriteLine("===== INFORMACION DE LA PERSONA =====");
         Console.WriteLine("Nombre: " + nombre);
         Console.WriteLine("Edad: " + edad);
         Console.WriteLine("Altura: " + altura + " metros");
         Console.WriteLine("Estudiante: " + estudiante);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Persona persona1 = new Persona();
+
+        persona1.nombre = "Carlos Diaz";
+        persona1.edad = 18;
+        persona1.altura = 1.70;
+        persona1.estudiante = true;
+
+        persona1.MostrarInformacion();
+
         Console.WriteLine();
+        Console.WriteLine("Presione una tecla para salir...");
+        Console.ReadKey();
     }
 }
